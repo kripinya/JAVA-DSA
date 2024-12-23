828c22a69222530c8514301ee71a5c528071e4be
Ananya Karn <kripinya@gmail.com>
2024-12-24 00:54:08 +0530

    Create StringLinearSearch.java

---
+++ StringLinearSearch.java
@@ -0,0 +1,18 @@
+public class arraySearch {
+    public static void main(String[] args){
+        String name = "Ananya";
+        int n = name.length();
+        String target = "y";
+        boolean found = false;
+        for(int i = 0; i<n; i++){
+            if(name.charAt(i) == target.charAt(0)){
+                System.out.println("Found at position " + (i+1));
+                found = true;
+                break;
+        } 
+        }
+        if(!found){
+            System.out.println("Not found");
+        }
+    }
+}
