f30ee88e90fe39a773bdecffa894666d8f4b286c
Ananya Karn <kripinya@gmail.com>
2025-05-26 00:02:12 +0530

    Add files via upload

--- reverse_a_string.java
+++ reverse_a_string.java
@@ -1,19 +1,11 @@
-//to reverse the given string
-
 class Reverse {
     public static String reverse(String s){
-        String reversed = "";
-        for(int i = s.length() - 1; i >=0; i--){
-            reversed += s.charAt(i);
-        }
-        return reversed;
+        return new StringBuilder(s).reverse().toString();
     }
-    
 }
+
 public class reverse_a_string {
     public static void main(String[] args){
-    Reverse r = new Reverse();
-    String result = r.reverse("ananya");
-    System.out.println("reverse of string is:" + result);
+        System.out.println(Reverse.reverse("ananya")); // ✅ Correct static call
     }
 }
