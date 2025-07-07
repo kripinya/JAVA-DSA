a5a76e66231628ee7d3907891b3978e124b2eaad
Ananya Karn <kripinya@gmail.com>
2025-07-07 16:02:18 +0530

    Add files via upload

---
+++ PrintTable2.java
@@ -0,0 +1,21 @@
+//print a table with the number n
+//recursive approach
+import java.util.Scanner;
+public class PrintTable2{
+    static void printtable(int n, Integer... val){
+        int i = 1;
+        if(val.length != 0)
+        i = val[0];
+        if(i == 11)
+        return;
+        System.out.println(n + " * " + i + " = " + n*i);
+        i++;
+        printtable(n, i);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printtable(n);
+        sc.close();
+    }
+}
