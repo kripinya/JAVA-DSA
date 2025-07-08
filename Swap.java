0a934ee38756c8ac4a5bde8495947ade763c261b
Ananya Karn <kripinya@gmail.com>
2025-07-08 10:34:25 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+//naive approach- Using Third Variable
+public class Swap {
+    public void swap(int a, int b){
+        int x = a;
+        a = b;
+        b = x;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int a = sc.nextInt();
+        System.out.println("enter another number: ");
+        int b = sc.nextInt();
+        Swap s = new Swap();
+        s.swap(a, b);
+        sc.close();
+    }
+}
---
+++ Swap2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Swap2 {
+    public void swap(int a, int b){//let a= 2, b = 3 
+        a = a + b;//5
+        b = a - b;//2
+        a = a -b;//3
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int a = sc.nextInt();
+        System.out.println("enter another number: ");
+        int b = sc.nextInt();
+        Swap2 s = new Swap2();
+        s.swap(a, b);
+        sc.close();
+    }
+}
